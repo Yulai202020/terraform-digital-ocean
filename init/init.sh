eebc769ea58deedb1df703835e719353a7b7bcc6
Yulai202020 <ngmyulai@gmail.com>
2024-02-06 21:06:25 +0500

    first commit

--- init/init.sh
+++ init/init.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
 
+### Install requiremets
+
 apt update
-apt install -y nginx
 apt install -y git-all
 
+apt install -y python3
+apt install -y python3-pip
+
+pip3 install uvicorn
+
+### Export Vars
+
+export service_name="yulai_api"
+export project_name="simple_project_fastapi"
+export git_project_link="https://github.com/Yulai202020/$project_name.git"
+
+### Copy Project
+
+cd /opt
+git clone $git_project_link
+
+cd $project_name
+pip install -r requirement.txt
+
+### Create Service
+
+sudo adduser --system --no-create-home ngm
+sudo cp $service_name.service /etc/systemd/system/
+
+### Start Service
+
+sudo systemctl daemon-reload
+sudo systemctl enable $service_name.service
+sudo systemctl start $service_name.service
+
+## Install Nginx
+
+sudo apt -y install nginx
 systemctl enable nginx
 systemctl start nginx
 
-hostname > /var/www/html/index.html
+# ### Start Nginx
+
+sudo cp $service_name.conf /etc/nginx/sites-available/
+sudo ln -sf /etc/nginx/sites-available/$service_name.conf /etc/nginx/sites-enabled/$service_name.conf
+sudo systemctl restart nginx
--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,19 @@
 terraform {
   required_version = ">= 0.14"
+  
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
+
+  # cloud {
+  #   organization = "yulai202020-org"
+  #   workspaces {
+  #     name = "digitalocean"
+  #   }
+  # }
 }
 
 provider "digitalocean" {
